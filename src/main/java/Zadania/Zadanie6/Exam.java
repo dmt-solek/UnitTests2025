7a4e40f70b6820cebc8e9ffc954731dec56c504f
Wioletta Solek <dmt.solek@lo5.bielsko.pl>
2025-04-03 20:52:04 +0200

    dodanie zadania 6

--- src/main/java/Zadania/Zadanie6/Exam.java
+++ src/main/java/Zadania/Zadanie6/Exam.java
@@ -1,8 +1,57 @@
 package Zadania.Zadanie6;
 public class Exam{
-    public static void main (String args[])
-        {
-        }
-        
+    public static void main (String args[]) {
     }
+
+        public static String calculateExam(String answers){
+
+            String[] splitAnswers = answers.split(";");
+
+            int point;
+            int result = 0;
+
+            for (int i=0; i < splitAnswers.length; i++) {
+
+                String odpowiedz = splitAnswers[i];
+
+                if (odpowiedz.equals("B")) {
+                    point = 3;
+                }
+                else if (odpowiedz.equals("_")) {
+                    point = -1;
+                }
+                else {
+                    point = -4;
+                }
+                result = result + point;
+            }
+
+            //Przeliczenie ilosci punktow na %
+            int procent = (result * 100) / 30;
+            System.out.println(procent + "%");
+
+            String message = null;
+        
+            if (procent >= 91) {
+                message = "bardzo dobry";
+            }
+            else if (procent < 91 && procent >= 81) {
+                message = "dobry plus";
+            }
+            else if (procent < 81 && procent >= 71) {
+                message = "dobry";
+            }
+            else if (procent < 71 && procent >= 61) {
+                message = "dostateczny plus";
+            }
+            else if (procent < 61 && procent >= 51) {
+                message = "dostateczny";
+            }
+            else {
+                message = "niedostateczny";
+            }
+
+        return message;
+        }
+}
   
--- src/test/java/Zadania/Zadanie6/ExamTest.java
+++ src/test/java/Zadania/Zadanie6/ExamTest.java
@@ -1,22 +1,46 @@
 package Zadania.Zadanie6;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-
 public class ExamTest {
     @ParameterizedTest
-    @ValueSource(strings={"A;A;A;A;A;A;A;A;A;A;"})
+    @ValueSource(strings={
+        "B;B;B;B;B;B;B;B;B;B", //bardzo dobry
+        "B;B;B;B;B;B;B;B;B;_", //dobry plus
+        "B;B;B;B;B;B;B;B;B;A", //dobry
+        "B;B;B;B;B;B;B;B;C;_", //dostateczny plus
+        "B;B;B;B;B;B;B;B;A;D", //dostateczny
+        "B;B;B;B;B;B;B;A;_;_" //niedostateczny
+    })
     public void examTestidyTest(String answers){
         //Arrange
         Exam exam = new Exam();
         //Act 
-        // String result = exam.calculateExam(answers);
+        String result = exam.calculateExam(answers);
         //Assert
-       // assertEquals("bardzo dobry", result);
-
+        assertEquals(testResults(answers), result);
     }
 
-
+    private String testResults(String answers) {
+        switch (answers) {
+            case "B;B;B;B;B;B;B;B;B;B":
+                return "bardzo dobry";               
+            case "B;B;B;B;B;B;B;B;B;_":
+                return "dobry plus";               
+            case "B;B;B;B;B;B;B;B;B;A":
+                return "dobry";               
+            case "B;B;B;B;B;B;B;B;C;_":
+                return "dostateczny plus";               
+            case "B;B;B;B;B;B;B;B;A;D":
+                return "dostateczny";               
+            case "B;B;B;B;B;B;B;A;_;_":
+                return "niedostateczny";               
+            default:
+                return "Nie chciales testowac takiej kombinacji!";
+        }
     }
+}
 
